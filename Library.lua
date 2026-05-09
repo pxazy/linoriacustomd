ef119f67c21f3d3b7a390f482411cb7431c3b6b6
pxazy <glebfnaf33@gmail.com>
2026-05-09 10:25:16 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3000,7 +3000,7 @@ function Library:CreateWindow(...)
         BackgroundColor3 = Library.BackgroundColor;
         BorderColor3 = Library.OutlineColor;
         Position = UDim2.new(0, 8, 0, 25);
-        Size = UDim2.new(1, -16, 1, -33);
+        Size = UDim2.new(1, -16, 1, -31);
         ZIndex = 1;
         Parent = Inner;
     });
