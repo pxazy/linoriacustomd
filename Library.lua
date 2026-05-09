39386b0372a16b7bb6c0d0123c5647297635e731
pxazy <glebfnaf33@gmail.com>
2026-05-09 08:51:12 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2960,15 +2960,16 @@ function Library:CreateWindow(...)
     };
 
     local Outer = Library:Create('Frame', {
-        AnchorPoint = Config.AnchorPoint,
-        BackgroundColor3 = Color3.new(0, 0, 0);
-        BorderSizePixel = 0;
-        Position = Config.Position,
-        Size = Config.Size,
-        Visible = false;
-        ZIndex = 1;
-        Parent = ScreenGui;
-    });
+    AnchorPoint = Config.AnchorPoint,
+    BackgroundColor3 = Color3.new(0, 0, 0);
+    BorderSizePixel = 0;
+    Position = Config.Position,
+    Size = Config.Size,
+    Visible = false;
+    ZIndex = 1;
+    ClipsDescendants = true;
+    Parent = ScreenGui;
+});
 
     Library:MakeDraggable(Outer, 25);
 
