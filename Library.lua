e5ef0caa48ef65a895da3b47fc099d429a14076f
pxazy <glebfnaf33@gmail.com>
2026-05-09 13:30:05 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2961,10 +2961,10 @@ function Library:CreateWindow(...)
 
     local Outer = Library:Create('Frame', {
         AnchorPoint = Config.AnchorPoint,
-        BackgroundColor3 = Color3.new(0, 0, 0);
-        BorderSizePixel = 0;
+        BackgroundColor3 = Library.BackgroundColor,
+        BorderSizePixel = 0,
         Position = Config.Position,
-        Size = Config.Size,
+        Size = UDim2.fromOffset(550, 500),
         Visible = false;
         ZIndex = 1;
         Parent = ScreenGui;
@@ -2973,15 +2973,14 @@ function Library:CreateWindow(...)
     Library:MakeDraggable(Outer, 25);
 
     local Inner = Library:Create('Frame', {
-        BackgroundColor3 = Library.MainColor;
-        BorderColor3 = Library.AccentColor;
-        BorderMode = Enum.BorderMode.Inset;
-        Position = UDim2.new(0, 1, 0, 1);
-        Size = UDim2.new(1, -2, 1, -2);
-        ZIndex = 1;
+        BackgroundColor3 = Library.MainColor,
+        BorderSizePixel = 0,
+        Position = UDim2.new(0, 0, 0, 0),
+        Size = UDim2.new(1, 0, 1, 0),
+        ZIndex = 1,
         Parent = Outer;
     });
-
+    
     Library:AddToRegistry(Inner, {
         BackgroundColor3 = 'MainColor';
         BorderColor3 = 'AccentColor';
