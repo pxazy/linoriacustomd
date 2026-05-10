9bf974e3eb3762a79c4ea0188965b0fac7fa23c9
pxazy <glebfnaf33@gmail.com>
2026-05-10 13:34:50 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3081,8 +3081,8 @@ function Library:CreateWindow(...)
     local TabContainer = Library:Create('Frame', {
         BackgroundColor3 = Library.MainColor;
         BorderColor3 = Library.OutlineColor;
-        Position = UDim2.new(0, 8, 0, 30); 
-        Size = UDim2.new(1, -16, 1, -38);
+        Position = UDim2.new(0, 133, 0, 8);
+        Size = UDim2.new(1, -141, 1, -16);
         ZIndex = 2;
         ClipsDescendants = true;
         Parent = MainSectionInner;
