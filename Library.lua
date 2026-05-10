3d1e1115e820d36b6ef32a93acc35c09c4fa8498
pxazy <glebfnaf33@gmail.com>
2026-05-10 13:25:38 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3105,7 +3105,7 @@ function Library:CreateWindow(...)
         local TabButtonWidth = Library:GetTextBounds(Name, Library.Font, 16);
 
         local TabButton = Library:Create('Frame', {
-            BackgroundColor3 = Library.BackgroundColor;
+            BackgroundColor3 = Library.MainColor;
             BorderColor3 = Library.OutlineColor;
             Size = UDim2.new(1, 0, 0, 30);
             ZIndex = 1;
