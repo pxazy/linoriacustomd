2911146d219960fb78712c6146a6f0e89533b9c7
pxazy <glebfnaf33@gmail.com>
2026-05-15 16:21:10 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2848,8 +2848,8 @@ do
 
     local KeybindContainer = Library:Create('Frame', {
         BackgroundTransparency = 1;
-        Size = UDim2.new(1, 0, 1, -20);
-        Position = UDim2.new(0, 0, 0, 20);
+        Size = UDim2.new(1, 0, 1, -35);
+        Position = UDim2.new(0, 0, 0, 35);
         ZIndex = 1;
         Parent = KeybindInner;
     });
