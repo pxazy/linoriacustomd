1cd4587544f8028f8547d61947db2f609608ef50
pxazy <glebfnaf33@gmail.com>
2026-05-16 10:26:41 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -70,7 +70,7 @@ local Library = {
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
-    Font = Enum.Font.Code,
+    Font = fontAssetId and Font.new(fontAssetId) or Enum.Font.Code,
 
     OpenedFrames = {};
     DependencyBoxes = {};
